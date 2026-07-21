ddbcc26fdf1db8f89bf37532ded8d95eb1ca4f52
Naitik Dhiman <naitikdhiman284@gmail.com>
2026-07-21 17:33:22 +0530

    added login form styling

--- FRONTEND/src/features/auth/pages/Login.jsx
+++ FRONTEND/src/features/auth/pages/Login.jsx
@@ -1,12 +1,42 @@
+import React from "react"
+import "../auth.form.scss"
 
-
-function Login(){
-
-
+const Login = () => {
   return (
-     <div> Login </div>
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Enter email address"
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Enter password"
+            />
+          </div>
+
+          <button type="submit" className="button primary-button">
+            Login
+          </button>
+        </form>
+      </div>
+    </main>
   )
 }
 
-
 export default Login
+
+
+
