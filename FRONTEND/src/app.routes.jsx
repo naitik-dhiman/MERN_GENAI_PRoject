abb9e062fcf112727775a88a3c10e85578edde33
Naitik Dhiman <naitikdhiman284@gmail.com>
2026-08-04 23:05:04 +0530

    Implemented the hook layer

--- FRONTEND/src/app.routes.jsx
+++ FRONTEND/src/app.routes.jsx
@@ -1,17 +1,18 @@
-// importing the required essentials 
-import { createBrowserRouter } from "react-router";
-// create createBrowserRouter([...]): Takes an array of route objects. Each object defines a mapping between a URL path and a React component.
+import { createBrowserRouter, Navigate } from "react-router";
 import Login from "./features/auth/pages/Login.jsx";
 import Register from "./features/auth/pages/Register.jsx";
 
-// in react this comes [] within a logic function 
-export const router = createBrowserRouter ([
+export const router = createBrowserRouter([
     {
-        path : "/login" , 
-        element : <Login />
-    }, 
-    {
-        path : "/register" , 
-        element : <Register />
+        path: "/",
+        element: <Navigate to="/login" replace />,
     },
-])
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+]);
--- FRONTEND/src/features/auth/pages/Login.jsx
+++ FRONTEND/src/features/auth/pages/Login.jsx
@@ -1,23 +1,31 @@
-import React from "react"
+import React , {useState} from "react"
 import "../auth.form.scss"
 import {useNavigate , Link } from "react-router"
+import {useAuth} from "../hooks/useAuth" 
 
 const Login = () => {
+  const { loading, handleLogin } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-   
-  const handleSubmit = (e) =>{
-    e.preventdefault() 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await handleLogin({ email, password });
+  };
+
+  if (loading) {
+    return <main><h1>Loading.......</h1></main>;
   }
-
 
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input
+              onChange={(e) => setEmail(e.target.value)}
               id="email"
               type="email"
               name="email"
@@ -28,6 +36,7 @@ const Login = () => {
           <div className="input-group">
             <label htmlFor="password">Password</label>
             <input
+              onChange={(e) => setPassword(e.target.value)}
               id="password"
               type="password"
               name="password"
@@ -40,10 +49,10 @@ const Login = () => {
           </button>
         </form>
 
-        <p>Don't  have an account ? <Link to ={"/register"}> Register </Link> </p>
+        <p>Don't have an account? <Link to="/register">Register</Link></p>
       </div>
     </main>
-  )
+  );
 }
 
 export default Login
